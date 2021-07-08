aed5c94e7cf4842f93b0e5b7f2da647767024595
Md Amir Gauhar <gauharamir15@gmail.com>
2021-07-08 16:42:11 +0530

    update: created header & added input field

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  const [input, setInput] = useState("");
+  return (
+    <div className="App">
+      <header>
+        <h1>Emoji Interpreter</h1>
+        <input
+          type="text"
+          placeholder="enter the emoji..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </header>
+    </div>
+  );
 }
